4a32aa1fa8d59683cadcb0e6b736bc4acff7f779
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-02-04 10:52:44 -0500

    Creating e2e tests for Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -15,7 +13,5 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
